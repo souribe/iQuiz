d8458d74507b032e1d296d34b924f8c838e2bb4e
Benny Souriyadeth <bennys@uw.edu>
2018-02-10 13:41:40 -0800

    add tableview

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -8,8 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let pics = ["coffee", "pom", "wall"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pics.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ iQuiz/ViewControllerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ViewControllerTableViewCell.swift
+//  iQuiz
+//
+//  Created by Benny on 2/10/18.
+//  Copyright © 2018 Benny Souriyadeth. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
